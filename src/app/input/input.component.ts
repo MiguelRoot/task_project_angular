9e45a5292b261f041419df6b10b97b5cf0d60f31
Alejandra2199 <alejandramiluska21@gmmail.com>
2024-01-26 22:53:56 -0500

    add input

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit, NgModule} from '@angular/core';
+import { FormControl, NgModel, ReactiveFormsModule} from '@angular/forms';
 import { TaskService } from '../task.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports:[ReactiveFormsModule, CommonModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
 })
+
 export class InputComponent implements OnInit {
   valueInput!: FormControl;
+  pok:any;
 
   constructor(private taksService: TaskService) {
     this.valueInput = new FormControl('');
   }
 
-  ngOnInit() { }
+  async ngOnInit(): Promise<void> {
+    this.pok = await this.taksService.getPokemon();
+   }
 
   addTask() {
     const value = this.valueInput.value;
